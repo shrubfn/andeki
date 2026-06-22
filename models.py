76bd22389bab7235919ea129d3a40019d605ec92
tim talam <timotaljm@gmail.com>
2026-06-23 02:14:31 +1000

    added email

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(200), nullable=False)
 
     #lets user objects have many relations to 
